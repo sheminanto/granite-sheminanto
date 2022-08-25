75131bc5cda19d249dc1f5317e63a2a928c823db
sheminanto <sheminanto@gmail.com>
2022-08-25 18:36:07 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import { setAuthHeaders } from "apis/axios";
-import { initializeLogger } from "./common/logger";
-import authAPI from "apis/auth";
 
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import { setAuthHeaders } from "apis/axios";
+
+import { initializeLogger } from "./common/logger";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
