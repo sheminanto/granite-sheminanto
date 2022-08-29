90b82fe9169e7d34fb1f3e72173e409af1e36bf7
sheminanto <sheminanto@gmail.com>
2022-08-29 12:02:24 +0530

    Added slug to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,6 @@ class TasksController < ApplicationController
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
+    # @task = Task.find_by(identifier_name: params[:identifier_name])bundle exec rails generate migration MakeSlugNotNullable
   end
 end
