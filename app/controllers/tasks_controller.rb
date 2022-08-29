e8b81cf2267ad91aafd5811a005cd2f6fae0579e
sheminanto <sheminanto@gmail.com>
2022-08-29 15:17:08 +0530

    Implemented create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,17 @@ class TasksController < ApplicationController
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
-    # @task = Task.find_by(identifier_name: params[:identifier_name])bundle exec rails generate migration MakeSlugNotNullable
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    respond_with_success("Task was successfully created")
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { setAuthHeaders } from "apis/axios";
 import Dashboard from "components/Dashboard";
 import PageLoader from "components/PageLoader";
+import CreateTask from "components/Tasks/Create";
 
 import { initializeLogger } from "./common/logger";
 
@@ -29,6 +30,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact component={CreateTask} path="/tasks/create" />
         <Route exact component={Dashboard} path="/dashboard" />
       </Switch>
     </Router>
